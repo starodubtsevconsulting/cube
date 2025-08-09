c91a4c7419f9c2a1d789b95aae261ce466666dae
starodubtsevconsulting <sergii@starodubtsev.consulting>
2025-08-09 15:20:46 -0400

    domain specific language with vertices defined
    updated docs with the notes

--- src/cube.js
+++ src/cube.js
@@ -4,6 +4,29 @@ function drawCube() {
         console.error('Canvas context not found');
         return;
     }
+    // domain specific language:
+    // - vertices - array of points
+    // - edges - array of lines (that connect vertices)
+    // - faces - array of polygons (surfaces, rectangles in this case)
+
+    // Cube has 8 vertices (dots, points)
+    vertices = [
+        // front
+        // bottom
+        { x: 0,   y: 0, z: 100 }, // bottom left
+        { x: 100, y: 0, z: 100 }, // bottom right
+        // top
+        { x: 0,   y: 100, z: 100 }, // top left
+        { x: 100, y: 100, z: 100 }, // top right
+
+        // back
+        // bottom
+        { x: 0,   y: 0, z: 200 }, // bottom left
+        { x: 100, y: 0, z: 200 }, // bottom right
+        // top
+        { x: 0,   y: 100, z: 200 }, // top left
+        { x: 100, y: 100, z: 200 }, // top right
+    ];
 
     console.log('Drawing cube...');
 }
